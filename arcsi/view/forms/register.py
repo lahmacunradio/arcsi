d8f86878323198a9c3a28935683826bd6003763d
pvj <alavela05@gmail.com>
2020-07-10 16:19:31 +0200

    templates -- custom register form w/ butt fields

--- arcsi/view/forms/register.py
+++ arcsi/view/forms/register.py
@@ -3,5 +3,6 @@ from flask_security.forms import RegisterForm
 
 
 class ButtRegisterForm(RegisterForm):
+    name = StringField("Username", [])
     butt_user = StringField("Butt username", [])
     butt_pw = StringField("Butt password", [])
