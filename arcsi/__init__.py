d9f0ce0128bd47cfe6f98ca1132923fbd2d42b45
pvj <alavela05@gmail.com>
2020-07-10 16:13:19 +0200

    __init__ for our app

--- arcsi/__init__.py
+++ arcsi/__init__.py
@@ -1,28 +1,31 @@
 import os
 
 from flask import Flask
+from flask_security import Security, SQLAlchemySessionUserDatastore
 from flask_migrate import Migrate
 
-from arcsi.model.item import db
+from arcsi.model import db, item, role, show, user
+from arcsi.view.forms.register import ButtRegisterForm
 
 migrate = Migrate()
 
-# TODO test config
-def create_app():
+# TODO create separate: dev, prod, test config -- from base config
+def create_app(config_file):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="ergya",
-        SQLALCHEMY_DATABASE_URI="postgresql://localhost/<dbname>",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        UPLOAD_FOLDER="/example/path",
-    )
+    app.config.from_pyfile(config_file)
 
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
+        # TODO handle exception correctly
         pass
+
+    user_store = SQLAlchemySessionUserDatastore(db.session, user.User, role.Role)
+    # TODO w/ user_store can create_role() etc.
+    # https://pythonhosted.org/Flask-Security/api.html#flask_security.datastore.SQLAlchemyUserDatastore
+    security = Security(app, user_store, register_form=ButtRegisterForm)
 
     with app.app_context():
         db.init_app(app)
