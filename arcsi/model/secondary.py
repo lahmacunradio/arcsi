7690ffa48aaf04d605096484699062555abc2963
pvj <alavela05@gmail.com>
2021-12-12 14:21:50 +0100

    feat(tag): create tag models and relationships to shows, episodes

--- arcsi/model/secondary.py
+++ arcsi/model/secondary.py
@@ -6,6 +6,18 @@ items_shows = db.Table(
     db.Column("item_id", db.Integer(), db.ForeignKey("items.id")),
 )
 
+tags_items = db.Table(
+    "tags_items",
+    db.Column("item_id", db.Integer(), db.ForeignKey("items.id")),
+    db.Column("tag_id", db.Integer(), db.ForeignKey("tags.id")),
+)
+
+tags_shows = db.Table(
+    "tags_shows",
+    db.Column("show_id", db.Integer(), db.ForeignKey("shows.id")),
+    db.Column("tag_id", db.Integer(), db.ForeignKey("tags.id")),
+)
+
 shows_users = db.Table(
     "shows_users",
     db.Column("user_id", db.Integer(), db.ForeignKey("users.id")),
