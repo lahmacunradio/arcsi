215b5c5a3f6f94161eb57f2ce94a59f09a3bae45
pvj <alavela05@gmail.com>
2021-12-13 16:35:52 +0100

    feat(tags): Added model, api, view, template and db migration

--- arcsi/model/tag.py
+++ arcsi/model/tag.py
@@ -4,14 +4,15 @@ from . import db
 class Tag(db.Model):
     __tablename__ = "tags"
     id = db.Column(db.Integer, primary_key=True)
-    display_name = db.Column(db.String(), nullable=False)
+    display_name = db.Column(db.String(66), nullable=False, unique=True)
+    clean_name = db.Column(db.String(66), nullable=False)
     icon = db.Column(db.String())
-    uploader = db.Column(db.String(), default="arcsi", nullable=False)
-    uploaded_at = db.Column(
+    created_at = db.Column(
         db.DateTime, default=db.func.current_timestamp(), nullable=False
     )
 
+
     def __repr__(self):
-        return "<Tag #{} -- {}>".format(
-            self.display_name, self.uploader
+        return "<Tag #{}>".format(
+            self.clean_name
         )
