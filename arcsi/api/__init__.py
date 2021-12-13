03872438620f95c374399355274d791ce3c3a61d
pvj <alavela05@gmail.com>
2021-12-13 18:02:10 +0100

    fix(api): add tag module to api package

--- arcsi/api/__init__.py
+++ arcsi/api/__init__.py
@@ -5,6 +5,7 @@ arcsi = Blueprint("arcsi", __name__, url_prefix="/arcsi")
 from .item import *
 from .role import *
 from .show import *
+from .tag import *
 from .user import *
 from .data import *
 
