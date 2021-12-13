de2653dd03518bf66f04693440e65c806281e303
pvj <alavela05@gmail.com>
2021-12-13 17:47:31 +0100

    feat(tags): add tags schema and basic collection endpoint

---
+++ arcsi/api/tag.py
@@ -0,0 +1,23 @@
+from marshmallow import fields, post_load, Schema
+
+from arcsi.api import arcsi
+from arcsi.model.tag import Tag
+
+
+class TagDetailsSchema(Schema):
+    id = fields.Int()
+    display_name = fields.Str(required=True)
+    
+    @post_load
+    def make_tag(self, data, **kwargs):
+        return Tag(**data)
+
+tags_details_schema = TagDetailsSchema()
+tags_partial_schema = TagDetailsSchema(partial=True)
+many_tags_schema = TagDetailsSchema(many=True)
+
+@arcsi.route("/tags", methods=["GET"])
+@arcsi.route("/tags/all", methods=["GET"])
+def list_tags():
+    tags = Tag.query.all()
+    return many_tags_schema.dumps(tags)
