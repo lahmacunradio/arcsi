5553c955feb969f024249d0a09c0228d3933e45d
tuz666 <frenyo.endre@gmail.com>
2022-04-10 14:29:49 +0200

    delete entrypoint.sh

--- entrypoint_ci.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python3 -m flask db upgrade
-gunicorn --worker-tmp-dir /dev/shm -w 1 --worker-class=gthread --threads 4 -b 127.0.0.1:5666 --log-file /app/aguni.log --log-level info "arcsi:create_app('../config_ci.py')"
--- run_ci.py
+++ run_ci.py
@@ -4,4 +4,4 @@ app = create_app("../config_ci.py")
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5666)
+    app.run(host="127.0.0.1", port=5000)
