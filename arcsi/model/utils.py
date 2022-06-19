e66f6194baf4d28bcafd7293cc50d09ef5a649a8
pvj <alavela05@gmail.com>
2022-06-19 15:08:01 +0200

    document(model util method) -- Try to explain what get_or_create does

--- arcsi/model/utils.py
+++ arcsi/model/utils.py
@@ -1,5 +1,10 @@
 from . import db
 
+""" Wrapper method for simple model calls.
+The utility method will return an initialised model object,
+either an existing db record or a new object initialised in place.
+Warning: The new models need to be peristed at method call site!
+"""
 def get_or_create(model, **kwargs):
   get = db.session.query(model).filter_by(**kwargs).first()
   if get:
