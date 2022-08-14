cfe29403b43b376f64e45c77c9485cfe0a57c430
pvj <alavela05@gmail.com>
2022-08-14 15:13:07 +0200

    wip

--- arcsi/api/show.py
+++ arcsi/api/show.py
@@ -66,7 +66,7 @@ class ShowDetailsSchema(Schema):
     tags = fields.List(
         fields.Nested(
             "TagDetailsSchema",
-            only=("display_name",),
+            only=("id", "display_name",),
         )
     )
 
@@ -78,7 +78,8 @@ class ShowDetailsSchema(Schema):
 show_schema = ShowDetailsSchema()
 show_archive_schema = ShowDetailsSchema(only=("id", "active", "name", "description", "cover_image_url", 
                                                     "day", "start", "end", "frequency", "language",
-                                                    "playlist_name", "archive_lahmastore_base_url", "archive_mixcloud_base_url", "items"))
+                                                    "archive_lahmastore_base_url", "archive_mixcloud_base_url",
+                                                    "items", "playlist_name", "tags"))
 show_partial_schema = ShowDetailsSchema(partial=True)
 shows_schema = ShowDetailsSchema(many=True)
 shows_schedule_schema = ShowDetailsSchema(many=True, exclude=("items",))
