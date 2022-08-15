7922d8dc6750c3fb8621433355f7525bb632b4db
pvj <alavela05@gmail.com>
2022-08-15 18:51:36 +0200

    dev(model update): add new field contact address for shows

--- arcsi/model/show.py
+++ arcsi/model/show.py
@@ -8,6 +8,7 @@ class Show(db.Model):
     active = db.Column(db.Boolean, nullable=False, default=True)
     name = db.Column(db.String(), unique=True, nullable=False)
     description = db.Column(db.String())
+    contact_address = db.Column(db.String, unique=False, nullable=True)
     language = db.Column(db.String(5), default="hu_hu")
     # TODO rename to image_url
     cover_image_url = db.Column(db.String())
