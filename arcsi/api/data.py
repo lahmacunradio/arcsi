c788a893dd010adb3de4a8d8cc9bac963696dee8
tuz666 <frenyo.endre@gmail.com>
2022-12-12 17:38:45 +0100

    refactor to be more logical

--- arcsi/api/data.py
+++ arcsi/api/data.py
@@ -12,32 +12,40 @@ headers = {"Content-Type": "application/json"}
 @roles_required("guest")
 def uploaded_episodes_in_last_x_days():
     date_metadata = request.form.to_dict()
-    from_date = date_metadata['from_date']
-    given_date = datetime.strptime(from_date, '%Y-%m-%d')
-    if (given_date > datetime.today()):
+    end_date = date_metadata['end_date']
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    if (end_date > datetime.today()):
         return make_response("Back to the future?", 418, headers)
-    last_days = int(date_metadata['last_days'])
-    if (last_days < 1):
+
+    days = int(date_metadata['days'])
+    if (days < 1):
         return make_response("Please, be postive!", 400, headers)
+
+    start_date = end_date - timedelta(days=days)
     episodes_count = Item.query.filter(
-        given_date - timedelta(days=last_days - 1) <= Item.play_date
-        ).filter(Item.play_date <= given_date).count()
-    ret = {"uploaded_episodes_in_last_%d_days" % (last_days): episodes_count}
+        start_date < Item.play_date
+        ).filter(Item.play_date <= end_date).count()
+
+    ret = {"uploaded_episodes_between_%s_%s" % (start_date.date(), end_date.date()): episodes_count}
     return make_response(jsonify(ret), 200, headers)
 
 @arcsi.route("/data/episodes_uploaded_weekly", methods=["POST"])
 @roles_required("guest")
 def uploaded_episodes_in_last_x_weeks():
     date_metadata = request.form.to_dict()
-    from_date = date_metadata['from_date']
-    given_date = datetime.strptime(from_date, '%Y-%m-%d')
-    if (given_date > datetime.today()):
+    end_date = date_metadata['end_date']
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    if (end_date > datetime.today()):
         return make_response("Back to the future?", 418, headers)
-    last_weeks = int(date_metadata['last_weeks'])
-    if (last_weeks < 1):
+
+    weeks = int(date_metadata['weeks'])
+    if (weeks < 1):
         return make_response("Please, be postive!", 400, headers)
+
+    start_date = end_date - timedelta(weeks=weeks)
     episodes_count = Item.query.filter(
-        given_date - timedelta(weeks=last_weeks) + timedelta(days=1) <= Item.play_date
-        ).filter(Item.play_date <= given_date).count()
-    ret = {"uploaded_episodes_in_last_%d_weeks" % (last_weeks): episodes_count}
+        start_date < Item.play_date
+        ).filter(Item.play_date <= end_date).count()
+
+    ret = {"uploaded_episodes_between_%s_%s" % (start_date.date(), end_date.date()): episodes_count}
     return make_response(jsonify(ret), 200, headers)
