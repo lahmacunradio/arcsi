40319e2b71f380ec6fc0d7db0c1f76b11382f763
pvj <alavela05@gmail.com>
2023-01-29 23:21:36 +0100

    dev(api/shows): Contact_address field schema validation and POST request handling

--- arcsi/api/show.py
+++ arcsi/api/show.py
@@ -27,6 +27,7 @@ class ShowDetailsSchema(Schema):
     language = fields.Str(max=5)
     playlist_name = fields.Str()
     frequency = fields.Int(min=1, max=1)
+    contact_address = fields.Email()
     week = fields.Int()
     day = fields.Int()
     start = fields.Time()
@@ -72,12 +73,12 @@ class ShowDetailsSchema(Schema):
 
 
 show_schema = ShowDetailsSchema()
-show_archive_schema = ShowDetailsSchema(only=("id", "active", "name", "description", "cover_image_url", 
+show_archive_schema = ShowDetailsSchema(only=("id", "active", "name", "description", "contact_address", "cover_image_url", 
                                                     "day", "start", "end", "frequency", "language",
                                                     "playlist_name", "archive_lahmastore_base_url", "social_base_url", "items", "tags"))
 show_partial_schema = ShowDetailsSchema(partial=True)
 shows_schema = ShowDetailsSchema(many=True)
-shows_schedule_schema = ShowDetailsSchema(many=True, exclude=("items",))
+shows_schedule_schema = ShowDetailsSchema(many=True, exclude=("items", "contact_address"))
 shows_schedule_by_schema = ShowDetailsSchema(many=True, 
                                                     only=("id", "active", "name", "description", "cover_image_url", 
                                                     "day", "start", "end", "frequency", "language",
@@ -194,6 +195,7 @@ def add_show():
             name=show_metadata.name,
             description=show_metadata.description,
             social_base_url=show_metadata.social_base_url,
+            contact_address=show_metadata.contact_address,
             language=show_metadata.language,
             playlist_name=show_metadata.playlist_name,
             frequency=show_metadata.frequency,
@@ -284,6 +286,7 @@ def edit_show(id):
         show.active = show_metadata.active
         show.name = show_metadata.name
         show.description = show_metadata.description
+        show.contact_address = show_metadata.contact_address
         show.social_base_url=show_metadata.social_base_url
         show.language = show_metadata.language
         show.playlist_name = show_metadata.playlist_name
