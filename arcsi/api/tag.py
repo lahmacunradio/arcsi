2b2517b4cca90ce3750a27cd1d7e51e6291aa0cc
tuz666 <frenyo.endre@gmail.com>
2023-09-04 19:16:26 +0200

    Extend tags further, update Swagger

--- arcsi/api/tag.py
+++ arcsi/api/tag.py
@@ -2,6 +2,7 @@ from flask import make_response
 from flask_security import auth_token_required
 from marshmallow import fields, post_load, Schema
 
+from arcsi.handler.upload import DoArchive
 from arcsi.api import arcsi
 from arcsi.model.tag import Tag
 from .utils import find_request_params, normalise
@@ -11,7 +12,7 @@ class TagDetailsSchema(Schema):
     display_name = fields.Str(required=True, min=3)
     clean_name = fields.Str(dump_only=True)
     icon = fields.Str(dump_only=True)
-    items = fields.Nested("ItemDetailsSchema", many=True, only=("id", "name", "name_slug", "description", "image_url", "play_date"), dump_only=True)
+    items = fields.Nested("ItemDetailsSchema", many=True, only=("id", "name", "name_slug", "description", "image_url", "play_date", "shows"), dump_only=True)
     shows = fields.Nested("ShowDetailsSchema", many=True, only=("id", "name", "archive_lahmastore_base_url", "description", "cover_image_url"), dump_only=True)
 
     # TODO -- Tag count
@@ -21,7 +22,7 @@ class TagDetailsSchema(Schema):
         return Tag(**data)
 
 tags_details_schema = TagDetailsSchema()
-many_tags_schema = TagDetailsSchema(many=True)
+many_tags_schema = TagDetailsSchema(many=True, only=("id", "display_name", "clean_name"))
 
 headers = {"Content-Type": "application/json"}
 
@@ -35,8 +36,17 @@ def list_tags():
 @arcsi.route("/tag/<string:clean_tag>", methods=["GET"])
 @auth_token_required
 def view_tagged(clean_tag):
+    do = DoArchive()
     tag = Tag.query.filter_by(clean_name=clean_tag).first_or_404()
     if tag:
         for item in tag.items:
-            item.name_slug=normalise(item.name)    
+            item.name_slug=normalise(item.name)
+            item.image_url=do.download(
+                item.shows[0].archive_lahmastore_base_url, item.image_url
+            )
+        for show in tag.shows:
+            if show.cover_image_url:
+                show.cover_image_url = do.download(
+                    show.archive_lahmastore_base_url, show.cover_image_url
+                )
         return make_response(tags_details_schema.dumps(tag), 200, headers,)
