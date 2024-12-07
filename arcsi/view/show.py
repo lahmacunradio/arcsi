4773a40ec5e43d8ba0199984c226dde24efe0eca
tuz666 <frenyo.endre@gmail.com>
2024-12-08 00:45:39 +0100

    separate admin and host roles for show use case

--- arcsi/view/show.py
+++ arcsi/view/show.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_login import current_user
 from flask_security import login_required, roles_accepted, roles_required
 
 from arcsi.api import archon_list_shows, archon_view_show
@@ -33,4 +34,6 @@ def edit_show(id):
     show = archon_view_show(id)
     if (hasattr(show, 'status_code') and show.status_code == 404):
         return "Show not found"
+    if (not current_user.has_role("admin") and not next(filter(lambda show: show['id']==id, current_user.shows))):
+        return "You don't have access to edit this show!"
     return render_template("show/edit.html", show=show)
