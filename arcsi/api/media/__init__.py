d8966cc7dd134e3b3f69debb23535354b3024c13
pvj <alavela05@gmail.com>
2025-04-02 01:14:28 +0200

    dev(media): Handle media uuid de-serialisation
    Iterate on schema fields and error paths

--- arcsi/api/media/__init__.py
+++ arcsi/api/media/__init__.py
@@ -1,6 +1,8 @@
+from b64uuid import B64UUID
 from flask import Blueprint
-from marshmallow import fields, post_load, Schema
+from marshmallow import fields, post_dump, post_load, pre_dump, pre_load, Schema
 from arcsi.model.media import Media
+
 
 media = Blueprint("media", __name__, url_prefix="/media")
 
@@ -9,12 +11,22 @@ class MediaSimpleSchema(Schema):
     id = fields.Str()
     name = fields.Str(required=True, min=1)
     extension = fields.Str(required=True)
-    url = fields.Str(dump_only=True)
+    url = fields.Url(dump_only=True)
     external_storage = fields.Boolean(required=True)
     dimension = fields.Str(dump_only=True)
     created_at = fields.Date(dump_only=True)
     source = fields.Str()
     size = fields.Int(required=True)
+
+    @post_dump
+    def make_front_face(self, data, **kwargs):
+        data["id"] = B64UUID(data["id"]).string
+        return data
+
+    @pre_load
+    def make_back_face(self, data, **kwargs):
+        if data.get("id"):
+            return str(B64UUID(data["id"]).uuid)
 
     @post_load
     def make_media(self, data, **kwargs):
