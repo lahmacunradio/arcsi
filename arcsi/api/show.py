4fde79095a16d5beb32db739186c0d40fa2df356
tuz666 <frenyo.endre@gmail.com>
2026-09-10 23:57:27 +0200

    align code, api doc and tests to week int -> array<int> change

--- arcsi/api/show.py
+++ arcsi/api/show.py
@@ -181,7 +181,8 @@ def archon_list_shows():
 @auth_token_required
 def frontend_shows_schedule():
     week = request.args.get("week", 1, type=int)
-    shows = Show.query.filter(Show.week == week)
+    # TODO: week contains
+    shows = Show.query.filter(Show.week.any(week))
     return make_response(
         jsonify(get_active_shows_with_latest_item(shows, shows_schedule_schema)),
         200,
@@ -193,17 +194,7 @@ def frontend_shows_schedule():
 @auth_token_required
 def frontend_list_shows_for_schedule():
     week = request.args.get("week", 1, type=int)
-    shows = Show.query.filter(Show.week == week).filter(Show.active == True).all()
-    # TODO: change with DB migration
-    # current_day = datetime.today().isocalendar().weekday
-    # current_week = datetime.today().isocalendar().week
-    # abcd_week = current_week % 4
-    # shows = Show.query.filter(Show.active == True)
-    # shows_on_this_week = shows.filter(
-    #     (Show.week == (abcd_week + 1) and Show.day >= current_day)
-    # )
-    # shows_on_next_week = shows.filter(Show.week == abcd_week and Show.day < current_day)
-    # shows = shows_on_this_week.union(shows_on_next_week).all()
+    shows = Show.query.filter(Show.week.any(week)).filter(Show.active == True).all()
     for show in shows:
         get_show_cover(show)
     return shows_schedule_excluded_schema.dump(shows)
@@ -215,7 +206,7 @@ def frontend_list_shows_for_schedule():
 def frontend_list_shows_for_schedule_by():
     week = request.args.get("week", 1, type=int)
     day = request.args.get("day", 1, type=int)
-    shows = Show.query.filter_by(week=week).filter_by(day=day)
+    shows = Show.query.filter(Show.week.any(week)).filter(Show.day == day)
     return get_active_shows_with_latest_item(shows, shows_schedule_schema)
 
 
